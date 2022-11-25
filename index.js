e12475c059bbd9924a17267444d60a432aaf8b97
Anamul10 <anamul.hasan@mnsu.edu>
2022-11-24 20:17:45 -0500

    showed category items to UI from database

--- index.js
+++ index.js
@@ -17,15 +17,37 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 async function run() {
     try {
         const categoryCollection = client.db('usedProductsResale').collection('categoryNames');
-        // console.log(categoryCollection);
-        // const reviewCollection = client.db('fitnessTraining').collection('reviews');
+        const carCollection = client.db('usedProductsResale').collection('usedCars');
+        console.log(carCollection);
 
+        //category items
         app.get('/categoryNames', async (req, res) => {
             const query = {}
             const cursor = categoryCollection.find(query);
             const categoryNames = await cursor.toArray();
             res.send(categoryNames);
         })
+
+        //experiment
+        app.get('/usedCars', async (req, res) => {
+            const query = {}
+            const cursor = carCollection.find(query);
+            const usedCars = await cursor.toArray();
+            res.send(usedCars);
+        })
+
+        //trying
+        app.get('/usedCars/:id', async (req, res) => {
+            const id = req.params.cat_id;
+            console.log(id);
+            const query = { _id: ObjectId(id) };
+            const car = await carCollection.findOne(query);
+
+            res.send(car);
+        })
+
+        //([0-9a-fA-F]{24})
+
     }
     finally {
 
