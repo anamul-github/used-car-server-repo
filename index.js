ea6d9f6581aa401c8fb7e57d3db4f02819955117
Anamul10 <anamul.hasan@mnsu.edu>
2022-11-27 05:14:48 -0500

    added admin api to server

--- index.js
+++ index.js
@@ -99,6 +99,13 @@ async function run() {
             res.send(result);
         });
 
+        app.get('/users/admin/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const user = await usersCollection.findOne(query);
+            res.send({ isAdmin: user?.role === 'admin' });
+        })
+
 
 
         //([0-9a-fA-F]{24})
