dd4aae61940cfe5758f035c1f997822dfcbbb2a6
Anamul10 <anamul.hasan@mnsu.edu>
2022-11-28 02:45:57 -0500

    created new api to get all buyers, deleted unnecessary data

--- index.js
+++ index.js
@@ -133,6 +133,14 @@ async function run() {
             res.send({ isSeller: user?.role === 'seller' });
         })
 
+        // get all buyers
+        app.get('/users/buyer/:email', async (req, res) => {
+            const email = req.params.email;
+            const query = { email };
+            const user = await usersCollection.findOne(query);
+            res.send({ isBuyer: user?.role === 'buyer' });
+        })
+
         // add product api
         app.post("/products", async (req, res) => {
             const product = req.body;
